9672e4c33519afaa110987389e19da0681c5740e
tryptophannn <tryptophan.github@gmail.com>
2020-09-10 17:50:34 +0900

    Change let to const

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 import additionCalculator from './modules/addition-calculator';
 import taxCalculator from './modules/tax-calculator';
 
-let item1Price = 400;
-let item2Price = 600;
-let totalPrice = additionCalculator(item1Price, item2Price);
-let tax = 1.08;
-let priceIncludeTax = taxCalculator(totalPrice, tax);
+const item1Price = 400;
+const item2Price = 600;
+const totalPrice = additionCalculator(item1Price, item2Price);
+const tax = 1.08;
+const priceIncludeTax = taxCalculator(totalPrice, tax);
 
 console.log(priceIncludeTax);
