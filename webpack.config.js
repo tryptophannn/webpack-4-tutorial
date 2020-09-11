1b7c3698dcf270978d999e4d05cd4f7057d60da6
tryptophannn <tryptophan.github@gmail.com>
2020-09-11 18:55:28 +0900

    Fix syntax error

--- webpack.config.js
+++ webpack.config.js
@@ -15,73 +15,71 @@ module.exports = (env, argv) => {
   const IS_DEVELOPMENT = argv.mode === 'development';
 
   return {
-  // エントリーポイントの設定
-  entry: './src/js/app.js',
-  // 出力の設定
-  output: {
-    // 出力するファイル名
-    filename: 'bundle.js',
-    // 出力先のパス（絶対パスを指定する必要がある）
-    path: path.join(__dirname, 'public/js')
-  },
-  // ローダーの設定
-  module: {
-    rules: [
-      {
-        // ローダーの処理対象ファイル
-        test: /\.js$/,
-        // ローダーの処理対象から外すディレクトリ
-        exclude: /node_modules/,
-        use: [
-          {
-            // 利用するローダー
-            loader: 'babel-loader',
-            // ローダーのオプション
-            // 今回はbabel-loaderを利用しているため
-            // babelのオプションを指定しているという認識で問題ない
-            options: {
-              presets: [['@babel/preset-env', { modules: false }]]
+    // エントリーポイントの設定
+    entry: './src/js/app.js',
+    // 出力の設定
+    output: {
+      // 出力するファイル名
+      filename: 'bundle.js',
+      // 出力先のパス（絶対パスを指定する必要がある）
+      path: path.join(__dirname, 'public/js')
+    },
+    // ローダーの設定
+    module: {
+      rules: [
+        {
+          // ローダーの処理対象ファイル
+          test: /\.js$/,
+          // ローダーの処理対象から外すディレクトリ
+          exclude: /node_modules/,
+          use: [
+            {
+              // 利用するローダー
+              loader: 'babel-loader',
+              // ローダーのオプション
+              // 今回はbabel-loaderを利用しているため
+              // babelのオプションを指定しているという認識で問題ない
+              options: {
+                presets: [['@babel/preset-env', { modules: false }]]
+              }
             }
-          }
-        ]
-      },
-      {
-        // enforce: 'pre'を指定することによって
-        // enforce: 'pre'がついていないローダーより早く処理が実行される
-        // 今回はbabel-loaderで変換する前にコードを検証したいため、指定が必要
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-    ]
-  },
-  // プラグインの設定
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
-    })
-  ],
-  // developmentモードで有効になるdevtool: 'eval'を上書き
-  // developmentモードでビルドした時だけソースマップを出力する
-  devtool: IS_DEVELOPMENT ? 'source-map' : 'none',
-  // productionモードで有効になるoptimization.minimizerを上書きする
-  optimization: {
-    // developmentモードでビルドした場合
-    // minimizer: [] となるため、consoleは残されたファイルが出力される
-    // puroductionモードでビルドした場合
-    // minimizer: [ new TerserPlugin({... となるため、consoleを削除したファイルが出力される
-    minimizer: IS_DEVELOPMENT
-      ? []
-      : [
-        new TerserPlugin({
-          terserOptions: {
-            compress: { drop_console: true }
-          }
-        })
+          ]
+        },
+        {
+          // enforce: 'pre'を指定することによって
+          // enforce: 'pre'がついていないローダーより早く処理が実行される
+          // 今回はbabel-loaderで変換する前にコードを検証したいため、指定が必要
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader'
+        }
       ]
-  }
+    },
+    // プラグインの設定
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery'
+      })
+    ],
+    // developmentモードで有効になるdevtool: 'eval'を上書き
+    // developmentモードでビルドした時だけソースマップを出力する
+    devtool: IS_DEVELOPMENT ? 'source-map' : 'none',
+    // productionモードで有効になるoptimization.minimizerを上書きする
+    optimization: {
+      // developmentモードでビルドした場合
+      // minimizer: [] となるため、consoleは残されたファイルが出力される
+      // puroductionモードでビルドした場合
+      // minimizer: [ new TerserPlugin({... となるため、consoleを削除したファイルが出力される
+      minimizer: IS_DEVELOPMENT
+        ? []
+        : [
+            new TerserPlugin({
+              terserOptions: {
+                compress: { drop_console: true }
+              }
+            })
+          ]
+    }
+  };
 };
-
