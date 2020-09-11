1d8a9c9583448cba7bc32eea217c1e48283e31e0
tryptophannn <tryptophan.github@gmail.com>
2020-09-11 18:40:23 +0900

    Add IS_DEVELOPMENT ?

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 // プラグインを利用するためにwebpackを読み込んでおく
 const webpack = require('webpack');
 
+// optimization.minimizerを上書きするために必要なプラグイン
+const TerserPlugin = require('terser-webpack-plugin');
+
 // output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
 const path = require('path');
 
-module.exports = {
-  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
-  mode: 'development',
+module.exports = (env, argv) => {
+  // argv.modeにはwebpackを実行したmodeが格納されている
+  // 例えば webpack --mode development と実行すれば
+  // argv.mode には 'development' が格納されている
+  // そのためdevelopmentモードで実行したかどうかを判定できる
+  const IS_DEVELOPMENT = argv.mode === 'development';
+
+  return {
   // エントリーポイントの設定
   entry: './src/js/app.js',
   // 出力の設定
@@ -16,7 +24,6 @@ module.exports = {
     // 出力先のパス（絶対パスを指定する必要がある）
     path: path.join(__dirname, 'public/js')
   },
-  devtool: 'source-map',
   // ローダーの設定
   module: {
     rules: [
@@ -57,5 +64,24 @@ module.exports = {
       'window.jQuery': 'jquery'
     })
   ],
+  // developmentモードで有効になるdevtool: 'eval'を上書き
+  // developmentモードでビルドした時だけソースマップを出力する
+  devtool: IS_DEVELOPMENT ? 'source-map' : 'none',
+  // productionモードで有効になるoptimization.minimizerを上書きする
+  optimization: {
+    // developmentモードでビルドした場合
+    // minimizer: [] となるため、consoleは残されたファイルが出力される
+    // puroductionモードでビルドした場合
+    // minimizer: [ new TerserPlugin({... となるため、consoleを削除したファイルが出力される
+    minimizer: IS_DEVELOPMENT
+      ? []
+      : [
+        new TerserPlugin({
+          terserOptions: {
+            compress: { drop_console: true }
+          }
+        })
+      ]
+  }
 };
 
